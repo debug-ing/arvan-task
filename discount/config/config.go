f3ce8b726685ad993a6e5f63de9e25af86ac6b29
debug-ing <mahdi.bl4ck@gmail.com>
2024-06-22 02:01:25 +0330

    Edit port and dockerize

--- discount/config/config.go
+++ discount/config/config.go
@@ -18,8 +18,8 @@ func LoadConfig() *AppConfig {
 		panic("No .env file found")
 	}
 	return &AppConfig{
-		DatabaseURL: getEnvString("DATABASE_URL", "root:your_password@tcp(localhost:3306)/test1-go?charset=utf8mb4&parseTime=True&loc=Local"),
-		Port:        getEnvString("PORT", ":8080"),
+		DatabaseURL: getEnvString("DATABASE_URL", "mongodb://localhost:27017"),
+		Port:        getEnvString("PORT", "8080"),
 		WalletURL:   getEnvString("WALLET_URL", "http://localhost:8081"),
 	}
 }
